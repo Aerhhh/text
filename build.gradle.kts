b10c2766368cd6348051ef0cf4ac316bbef8494f
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-07-06 04:27:46 -0400

    Pin utils to 37dc4a8

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     // commit on collections (ece3042 onward) - older pins linked against the AtomicCollection
     // class form and surfaced LambdaConversionException at consumer link time.
     api("com.github.simplified-dev:collections") { version { strictly("6586657") } }
-    api("com.github.simplified-dev:utils") { version { strictly("5c6c96a") } }
+    api("com.github.simplified-dev:utils") { version { strictly("37dc4a8") } }
     api("com.github.simplified-dev:image") { version { strictly("4140130") } }
 
     // Gson - JsonObject / JsonElement used by TextSegment + friends
